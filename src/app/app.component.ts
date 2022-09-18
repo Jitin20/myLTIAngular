93a4a3eb1a47e11a9d059119f668a4de86a9597c
Jitin20 <jitinsharma2001@rediffmail.com>
2022-09-18 21:40:04 +0530

    This is the othr way to define a template with the code itself where the code and its view are on same page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template:`
+  // <h1>This is just for reference</h1>
+  // `,
+  //This is the othr way to define a template with the code itself where the code and its view are on same page
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp1';
+  title = 'myLTI';
 }
